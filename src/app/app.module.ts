f8b01b1f1b0b5d4e54b17791db72d4915ab2ec36
hanksbonniec <hanksbonniec@gmail.com>
2021-05-21 16:51:03 -0700

    added FormsModule, removed placeholder HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
@@ -10,7 +11,8 @@ import { HeroFormComponent } from './hero-form/hero-form.component';
     HeroFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
